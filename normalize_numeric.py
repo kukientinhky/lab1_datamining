22037ef4fc33f643ab597db169d0e4e3f64ad411
kiên lê <lexuankien122@gmail.com>
2023-10-28 21:41:08 +0700

    complete 7

---
+++ normalize_numeric.py
@@ -0,0 +1,46 @@
+import argparse
+import pandas as pd
+import math
+## command line
+parser = argparse.ArgumentParser(description="Fill missing values in CSV file")
+parser.add_argument("input_file", help="Input CSV file")
+parser.add_argument("--method", choices=["min_max", "z_score"], default="min_max", help="Imputation method: mean or median")
+parser.add_argument("--columns", nargs="+", help="Columns to impute")
+parser.add_argument("--out", dest="output_file", default="result.csv", help="Output CSV file")
+args = parser.parse_args()
+data = pd.read_csv(args.input_file)
+if args.method == "min_max":
+    a = input("Nhap giá trị đầu trong đoạn muốn chuẩn hóa:")
+    b = input("Nhap giá trị cuối trong đoạn muốn chuẩn hóa:")
+    a = int(a)
+    b = int(b)
+    for column in args.columns:
+        if column in data.columns:
+            mi = min(data[column])
+            ma = max(data[column])
+            index = 0
+            for value in data[column]:
+                if value == value:
+                    data.at[index, column] = round((((value - mi)/(ma - mi)) * (b - a) + a), 4)
+                    index += 1
+else:
+    for column in args.columns:
+        if column in data.columns:
+            sum = 0
+            sl = 0
+            sum_squared = 0
+            for value in data[column]:
+                if value == value:
+                    sl += 1
+                    sum += value
+            mean = sum/sl
+            for value in data[column]:
+                if value == value:
+                    sum_squared += (value - mean) ** 2
+            standard_deviation = math.sqrt(sum_squared / sl)
+            index = 0
+            for value in data[column]:
+                if value == value:
+                    data.at[index, column] = round(((value - mean)/standard_deviation), 4)
+                    index += 1
+data.to_csv(args.output_file)
